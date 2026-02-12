36f7f9c03065426b9a059e8488859752d105b56b
Abdul Wasio <shaikhabdulwasio@gmail.com>
2026-02-12 18:24:39 +0500

    Update menu.java

--- Mini_Project/menu.java
+++ Mini_Project/menu.java
@@ -10,7 +10,8 @@ public class menu {
         System.out.println("│ 3. Search Employee                │");
         System.out.println("│ 4. Update Employee                │");
         System.out.println("│ 5. View All Employees             │");
-        System.out.println("│ 6. Exit                           │");
+        System.out.println("│ 6. Generate Slip                  │");
+        System.out.println("│ 7. Exit                           │");
         System.out.println("└───────────────────────────────────┘");
     }
 }
